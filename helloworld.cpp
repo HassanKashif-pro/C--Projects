7080f32cda47af3f4f90c36963f870ac2fc25bd1
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2024-12-23 20:06:37 +0500

    Arithmetic operators in C++ just like in anyother language.

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
-#include <vector>
-
-typedef std::vector<std::pair<std::string, int>> PairList; // This massive line is the definition of PairList
-using text_1 = std::string;                                // now a days keyword "using" is mostly used instead of typedef as it work better with templates.
-using number_1 = int;
 
 int main()
 {
-    // typedef = reserved keyword used to create an additional name alias for a type.
-    // increases readability and prevents typos.
 
-    text_1 firstname = "BRO";
-    number_1 age = 21;
+    // There are arithmetic operators just like any other programming language  *, +, -, / and %
 
-    std::cout << firstname << std::endl;
-    std::cout << age << std::endl;
+    int a = 5;
+    int b = 10;
+
+    int students = 22;
+
+    // 1. Parantheces
+    // 2. Multiplication
+    // 3. Division
+    // 4. Addition & Subtraction
+
+    students += 1;
+    students -= 33;
+
+    std::cout << students;
 }
