ced56540ba31efb0bbe7f672a564df5d0c4b6e91
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2024-12-24 21:47:00 +0500

    Temperature converter in C++.

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,21 +4,46 @@ using namespace std;
 
 int main()
 {
-    // ternary operator ?= replacement to if/else statement
-    // condition ? expression1 : expression 2;
+    double temp;
+    char unit;
 
-    int grade = 75;
-    int num;
-    bool hungry;
+    cout << "--------TEMPERATURE CONVERTER--------" << endl;
 
-    grade >= 70 ? std::cout << "Pass" << "\n" : std::cout << "Fail" << "\n";
+    cout << "F is for Fahrenheit\n";
+    cout << "C is for Celsius\n";
+    cout << "K is for Kelvin\n";
 
-    cout << "Write any number: ";
-    cin >> num;
+    cout << "What Unit would you like to convert to?: ";
+    cin >> unit;
 
-    num % 2 == 0 ? cout << "Even" : cout << "Odd";
+    if (unit == 'F' || unit == 'f')
+    {
+        cout << "Enter temperature in Celsius: ";
+        cin >> temp;
 
-    cout << (hungry ? "Your R hungry" : "Your R Full");
+        temp = (1.8 * temp) + 32;
+        cout << "Temperature is " << temp << "F\n";
+    }
+
+    else if (unit == 'C' || unit == 'c')
+    {
+        cout << "Enter temperature in Fahrenheit: ";
+        cin >> temp;
+
+        temp = (temp - 32) / 1.8;
+        cout << "Temperature is " << temp << "C\n";
+    }
+    else if (unit == 'K' || unit == 'k')
+    {
+        cout << "Enter temperature in Fahrenheit: ";
+        cin >> temp;
+
+        temp = (temp - 32) * 5 / 9;
+        cout << "Temperature is " << temp << "K\n";
+    }
+
+    cout
+        << "-------------------------------------" << endl;
 
     return 0;
 }
