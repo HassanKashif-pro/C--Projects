86e15ba023b8970e1e130a9e343b36c9de809d90
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2024-12-25 10:50:19 +0500

    While loop OR infinite loop in C++

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,20 +6,17 @@ int main()
 {
 
     string name;
+    while (name.empty()) // a While loop runs the code until the condition remains True When it is False it is stopped.
+    {
+        cout << "Enter your name: ";
+        std::getline(std::cin, name);
+    }
+    cout << "Hello, " << name << endl;
 
-    cout << "Enter your name: ";
-    getline(cin, name);
-
-    name.length();             // This will return the length of the string name
-    name.empty();              // This will check if the input is empty
-    name.clear();              // This will clear the input on display
-    name.append("@Gmail.com"); // This will append / add a string after the any input string
-    name.at(0, 3);             // THis will return the character at the index while is specified
-    name.insert(2, "@");       // This will insert a character at the specified index
-    name.find(' ');            // This will return the index of the first occurrence of the specified character it can also be a space
-    name.erase(0, 3);          // This will erase the characters from the specified index to the end of the string
-
-    // MORE ON CPLUSPLUS.COM
+    while (1 == 1)
+    {
+        cout << " HELP ME ! ";
+    }
 
     return 0;
 }
