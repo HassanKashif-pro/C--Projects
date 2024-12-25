c1fc71e0499b85a606caa13ea2a09bc10a79792e
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2024-12-25 23:34:42 +0500

    Use of return statement in C++.

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
-#include <cmath>
 
-using namespace std;
+double square(double length);
 
-void happyBirthday(std::string name, int age); // We need a function declaration because we are using it before it is defined
-
-int main() // Function called should be written before the main function
+int main()
 {
-    std::string name = "Hassan";
-    int age = 21;
+    // return : returns a value back to the spot
+    // where the function was called from
 
-    happyBirthday(name, age); // Calling a function...call it as many times as you want
+    double length = 5.1;
+    double area = square(length);
+
+    std::cout << "Area: " << area << std::endl;
 
     return 0;
 }
-// function is a reuseable block of code
-
-void happyBirthday(std::string name, int age) // you can make other functions be aware of variable by passing in as a variable.
+double square(double length) // double is used instead of a void because we are returning a value of type double
 {
-    cout << "Happy Birthday to you!" << endl;
-    cout << "Happy Birthday to you!" << endl;
+    return length * length;
 }
