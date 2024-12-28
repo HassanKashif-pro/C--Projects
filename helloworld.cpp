a7185e5c8f0dee252e4f8cc325c1e8b9f090ae65
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2024-12-28 22:17:49 +0500

    Passing an array to a function in C++.

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 
+double getTotal(double prices[], int size);
 int main()
 {
-    // for each = that ease the traversal over an iterable data set.
+    double prices[] = {21.32, 42.10, 40.5, 12.5};
+    int size = sizeof(prices) / sizeof(double); // we cannot use the sizeof() in line 18 in the for loop
+    // because when an array is passed in a function it is passed as a pointer so we have to give the size.
+    double total = getTotal(prices);
 
-    int grades[] = {65, 43, 76, 89};
-
-    for (int grade : grades)
-    {
-        std::cout << grade << "\n";
-    }
+    std::cout << "$" << total;
 
     return 0;
+}
+double getTotal(double prices[], int size) // you can pass an array as an argument.
+{
+    double total = 0;
+
+    for (int i = 0; i < size; i++) // The size is used as the array is decayed as a pointer.
+    {
+        total += prices[i];
+    }
+    return total;
 }
