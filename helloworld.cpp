395d9a1930f1b6872de2edab47df808325498f36
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2024-12-30 14:18:51 +0500

    Main D/B pass by value and pass by reference.

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 
+void swap(std::string x, std::string y);
 int main()
 {
-    // memory address = a location in memory where data is stored
-    // a memory address can accessed with & (address-of operator)
+    // Pass by value
+    // Pass by reference
 
-    std::string name = "Bro";
-    int age = 21;
+    std::string x = "Kool-aid";
+    std::string y = "Water";
 
-    std::cout << &name << '\n';
-    std::cout << &name << '\n';
+    swap(x, y);
+
+    std::cout << x << std::endl;
+    std::cout << y << std::endl;
 
     return 0;
+}
+// We can use '&' to pass by reference as memory location
+void swap(std::string &x, std::string &y) // When we pass in variables as argument they are passed by value
+                                          // meaning we create copies of the same element and do not pass the original one.
+{
+    std::string temp;
+
+    temp = x;
+    x = y;
+    y = temp;
 }
