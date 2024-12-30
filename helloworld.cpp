eaa39dcff8f1a9a16ea237b80f625a82052e5671
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2024-12-30 14:02:06 +0500

    Memory addresses in C++

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,24 +2,14 @@
 
 int main()
 {
-    // 2D arrays = an array made up of multiple arrays as elements
-    // They help in representing a grid or matrix of data.
+    // memory address = a location in memory where data is stored
+    // a memory address can accessed with & (address-of operator)
 
-    std::string cars[3][4] = {{"Mustang", "Escape", "F-150"},
-                              {"Corvette", "Equinox", "Silverado"},
-                              {"Challenger", "Durango", "Ram 1500"}}; // 3 rows and 4 columns
+    std::string name = "Bro";
+    int age = 21;
 
-    int rows = sizeof(cars) / sizeof(cars[0]);
-    int columns = sizeof(cars[0]) / sizeof(cars[0][0]);
-
-    for (int i = 0; i < rows; i++) // This is how to iterate over a 2D array
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            std::cout << cars[i][j] << " ";
-        }
-        std::cout << "\n";
-    }
+    std::cout << &name << '\n';
+    std::cout << &name << '\n';
 
     return 0;
 }
