ea4a44dd00fbf2cb1627983c84b2ecdd6c94799c
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2024-12-30 18:42:12 +0500

    Const params in C++

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,28 +1,21 @@
 #include <iostream>
 
-void swap(std::string x, std::string y);
+void printInfo(const std::string &name, const int &age);
+
 int main()
 {
-    // Pass by value
-    // Pass by reference
+    std::string name = "Bro";
+    int age = 21;
 
-    std::string x = "Kool-aid";
-    std::string y = "Water";
-
-    swap(x, y);
-
-    std::cout << x << std::endl;
-    std::cout << y << std::endl;
+    printInfo(name, age);
 
     return 0;
 }
-// We can use '&' to pass by reference as memory location
-void swap(std::string &x, std::string &y) // When we pass in variables as argument they are passed by value
-                                          // meaning we create copies of the same element and do not pass the original one.
-{
-    std::string temp;
 
-    temp = x;
-    x = y;
-    y = temp;
+void printInfo(const std::string &name, const int &age)
+{
+    name = "";
+    age = 0;
+    std::cout << name << '\n';
+    std::cout << age << '\n';
 }
