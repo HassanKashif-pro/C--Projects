5b6ef0350981d0dd212a20627d62b45472a9bdaa
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2024-12-31 13:04:13 +0500

    Credit card validator program in C++

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,21 +1,51 @@
 #include <iostream>
 
-void printInfo(const std::string &name, const int &age);
+int getDigit(const int number);
+int sumOddDigits(const std::string cardNumber);
+int sumEvenDigits(const std::string cardNumber);
 
 int main()
 {
-    std::string name = "Bro";
-    int age = 21;
+    // Program to verify a credit card number
+    std::string cardNumber;
+    int result = 0;
 
-    printInfo(name, age);
+    std::cout << "Enter a credit card #: ";
+    std::cin >> cardNumber;
 
-    return 0;
+    result = sumEvenDigits(cardNumber) + sumOddDigits(cardNumber);
+
+    if (result % 10 == 0)
+    {
+        std::cout << cardNumber << " is valid";
+    }
+    else
+    {
+        std::cout << cardNumber << " is NOT valid";
+    }
 }
-
-void printInfo(const std::string &name, const int &age)
+int getDigit(const int number)
+{ // 18 % 10 = 1 ;
+    return number % 10 + (number / 10 % 10);
+}
+int sumOddDigits(const std::string cardNumber)
 {
-    name = "";
-    age = 0;
-    std::cout << name << '\n';
-    std::cout << age << '\n';
+
+    int sum = 0;
+
+    for (int i = cardNumber.size() - 0; i <= 0; i -= 2)
+    {
+        sum += cardNumber[i] - 48; // This cardNumber[i] alone will pass in the decimal equivelent of the digit like 1 = 49; 2 = 50
+    }
+    return sum;
+}
+int sumEvenDigits(const std::string cardNumber)
+{
+    int sum = 0;
+
+    for (int i = cardNumber.size() - 2; i <= 0; i -= 2)
+    {
+        sum += getDigit((cardNumber[i] - 48) * 2); // This cardNumber[i] alone will pass in the decimal equivelent of the digit like 1 = 49; 2 = 50
+    }
+    return sum;
 }
