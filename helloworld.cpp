49070e316b87d06878f6e4005be3b85426c0aa71
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2025-01-01 18:47:55 +0500

    Function templates in C++

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,41 +1,16 @@
 #include <iostream>
+template <typename T, typename U>
 
+// Use the 'auto' to show double dispatch
+
+auto max(T x, U y) // This is a function template where you can
+                   // set this type to T which is a lot of things like char, string, int...
+{
+    return (x > y) ? x : y;
+}
 int main()
 {
-    // Dynamic memory that is allocated after the program
-    // is already compiled & running.
-    // Use the 'new' operator to allocate
-    // memory on the heap.
-    //
-    //  Useful when u are unsure of how much memory we will
-    //  need. Makes our program more flexible.
-
-    int *pNum = NULL;
-    char *pGrades = NULL;
-    int size;
-
-    pNum = new int; // This is how to allocate your memory to the heap and not the stack.
-
-    *pNum = 20; // store any kind of data afterwards
-
-    std::cout << "How many grades are there?: ";
-    std::cin >> size;
-
-    pGrades = new char[size];
-
-    for (int i = 0; i < size; i++)
-    {
-        std::cout << "Enter grade " << i + 1 << ": ";
-        std::cin >> pGrades[i];
-    }
-    for (int i = 0; i < size; i++)
-    {
-        std::cout << pGrades[i] << " ";
-    }
-
-    delete[] pGrades;
-    delete pNum; // You need to clear the memory manually afterwards to
-                 // avoid memory leaks. use 'delete' whenever you use 'new'.
-
+    std::cout << max(1, 2.1) << '\n'; // Only accept all arguments of the same type .. inless you are using
+    // different types like T and U
     return 0;
 }
