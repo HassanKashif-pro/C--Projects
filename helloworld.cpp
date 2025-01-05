5315677ad1146a90d67c8d378a219e71c84e6253
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2025-01-05 15:25:56 +0500

    Passing structs as arguments.

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,21 +2,37 @@
 
 // struct = A structure that group related variables under one name.
 
-struct helloworld
+struct Car
 {
-    std::string name;
-    double gpa;
-    bool enrolled;
+    int year;
+    std::string model;
+    std::string color;
 };
+void printCar(Car &car);
 
 int main()
 {
-    helloworld student;
-    student.name = "John";
-    student.gpa = 3.5;
-    student.enrolled = true;
+    Car car1;
+    Car car2;
 
-    std::cout << "Name: " << student.name << std::endl;
+    car1.model = "Mustang";
+    car1.year = 1999;
+    car1.color = "red";
+
+    car2.model = "GTR";
+    car2.year = 2020;
+    car2.color = "blue";
+
+    std::cout << &car1 << '\n'; // This will print the memory address of car1
+    // which is not same as the one on line 33
+    printCar(car1); // Structs are pass by value
+    printCar(car2); // Structs are pass by value
 
     return 0;
+}
+void printCar(Car &car) // you need to add address-of to give the same
+// memory address to the function
+{
+    std::cout << &car << '\n';
+    std::cout << "Model: " << car.model << std::endl;
 }
